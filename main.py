fc7f4f9c5cafbaa0942612c35b5b842f6f6691da
hlcooll <834890137@qq.com>
2021-01-22 15:27:19 +0800

    updates Fri Jan 22 15:27:19 CST 2021

---
+++ main.py
@@ -0,0 +1,47 @@
+from minio import Minio
+from minio.error import S3Error
+from progress.prog import Progress
+
+minioClient = Minio('127.0.0.1:9000',
+                  access_key='hualei',
+                  secret_key='michael44',secure=False
+                  )
+
+# Create bucket.
+class minioCreateBucket(object):
+    #init
+    def __init__(self,newPersonName):
+        self.name=newPersonName
+        try:
+            minioClient.make_bucket(self.name, location="us-east-1")
+        except S3Error as err:
+            print(err)
+
+    # Upload data.
+    def upload(self,object,filename):
+        try:
+            minioClient.fput_object(self.name, object,
+                                    filename)
+        except FileNotFoundError as err:
+            print(err)
+        except S3Error as err:
+            print(err)
+
+    # Upload data with progress bar.
+    def uploadResult(self,object,filename):
+        result = minioClient.fput_object(
+            self.name, object, filename,
+            progress=Progress(),
+        )
+        print(
+            "created {0} object; etag: {1}, version-id: {2}".format(
+                result.object_name, result.etag, result.version_id,
+            ),
+        )
+
+if __name__ == '__main__':
+    object_name='sw.md'
+    filename_url="/Users/hualei/hugo/hlcooll.github.io/content/kubernetes/ceph/ceph.md"
+    miniB = minioCreateBucket("maylogs")
+    miniB.uploadResult(object_name,filename_url)
+
